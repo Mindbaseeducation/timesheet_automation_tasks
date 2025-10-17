28991d08b7eb938825ab16c47e382813b0fbdec3
Mindbaseeducation <arjun.v@mindbase.education>
2025-10-17 17:27:26 +0530

    Update tsa_adhoc_task1.py
    Updated the code to flag out only part-time mentors

--- tsa_adhoc_task1.py
+++ tsa_adhoc_task1.py
@@ -3,21 +3,24 @@ import pandas as pd
 import sqlite3
 from io import BytesIO
 
-st.title("Timesheet Automation Task 3: Mentor Workload Flagging (Exceeding 6 Hours/Day)")
+st.title("Timesheet Automation Task 3: Part-Time Mentors Workload Flagging (Exceeding 6 Hours/Day)")
 
 # Upload only 1 file
-uploaded_file = st.file_uploader("Upload Final Output file (CSV or Excel)", type=["csv", "xlsx"])
+t1 = st.file_uploader("Upload Final Output file (CSV or Excel)", type=["csv", "xlsx"])
+t2 = st.file_uploader("Upload Mentors File to flag only the part-time mentors (CSV or Excel)", type=["csv", "xlsx"])
 
-if uploaded_file:
-    # Load file
-    if uploaded_file.name.endswith("xlsx"):
-        df = pd.read_excel(uploaded_file)
-    else:
-        df = pd.read_csv(uploaded_file)
+if t1 and t2:
+    # Helper function to load CSV/Excel
+    def load_file(f):
+        return pd.read_excel(f) if f.name.endswith("xlsx") else pd.read_csv(f)
+
+    # Load all 2 files
+    df1, df2 = load_file(t1), load_file(t2)
 
     # Create SQLite in-memory DB
     conn = sqlite3.connect(":memory:")
-    df.to_sql("sharepoint_table", conn, index=False, if_exists="replace")
+    df1.to_sql("table1", conn, index=False, if_exists="replace")
+    df2.to_sql("table2", conn, index=False, if_exists="replace")
 
     # SQL Query
     query = """
@@ -25,12 +28,12 @@ WITH base1 AS (SELECT "Logged by",
         Date,
         SUM("Duration in minutes") AS "Total minutes",
         SUM("Duration in hours") AS "Total hours"
-    FROM sharepoint_table
-    WHERE "Logged by" NOT IN ('Claire Mangrum', 'Dr Fauzia Hasan Siddiqui', 'Dr. Rubi Garcha', 'Allison Houston', 'Erin Nelson', 'Thoywell Hemmings', 'Rakhshan Sharif')
+    FROM table1
+    WHERE "Logged by" NOT IN ('Claire Mangrum', 'Dr Fauzia Hasan Siddiqui', 'Dr. Rubi Garcha', 'Allison Houston', 'Erin Nelson', 'Thoywell Hemmings')
     GROUP BY 1,2)
 SELECT *,
-       CASE WHEN "Total hours" > 6 
-       THEN "Mentor spent more than 6 hours on the students on this day. Please review"
+       CASE WHEN "Total hours" > 6 AND "Logged by" IN (SELECT Mentor FROM table2 WHERE "Mentor Status" = "Part Time")
+       THEN "Part Time Mentor spent more than 6 hours on the students on this day. Please review"
        END AS "HQ Remark"
     FROM base1
     ORDER BY 1,2;
@@ -54,8 +57,6 @@ SELECT *,
     st.download_button(
         label="📥 Download Result as Excel",
         data=excel_data,
-        file_name="Mentor Flagged for > 6 hours.xlsx",
+        file_name="PT Mentor Flagged for more than 6 hours.xlsx",
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
-
-
