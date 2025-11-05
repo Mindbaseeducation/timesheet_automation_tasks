206554c2f7ba44284e5c353ca99f4dee28c0bc3b
Mindbaseeducation <arjun.v@mindbase.education>
2025-11-06 01:20:40 +0530

    Update tsa_student_summary.py
    Updated code to include table 4 and table 5

--- tsa_student_summary.py
+++ tsa_student_summary.py
@@ -9,26 +9,30 @@ st.title("Timesheet Automation Task 4: Consolidated Student Summary")
 t1 = st.file_uploader("Upload Final Output File (CSV or Excel)", type=["csv", "xlsx"])
 t2 = st.file_uploader("Upload Master Data File for Country Mapping (CSV or Excel)", type=["csv", "xlsx"])
 t3 = st.file_uploader("Upload Last month's MPR for Student 1:1 Session Mapping (CSV or Excel)", type=["csv", "xlsx"])
+t4 = st.file_uploader("Upload Last to last month's MPR for Student 1:1 Session Mapping (CSV or Excel)", type=["csv", "xlsx"])
+t5 = st.file_uploader("Upload Mentors File to flag only the part-time mentors (CSV or Excel)", type=["csv", "xlsx"])
 
-if t1 and t2 and t3:
+if t1 and t2 and t3 and t4 and t5:
     # Helper function to load CSV/Excel
     def load_file(f):
         return pd.read_excel(f) if f.name.endswith("xlsx") else pd.read_csv(f)
 
     # Load all 2 files
-    df1, df2, df3 = load_file(t1), load_file(t2), load_file(t3)
+    df1, df2, df3, df4, df5 = load_file(t1), load_file(t2), load_file(t3), load_file(t4), load_file(t5)
 
     # Create SQLite in-memory DB
     conn = sqlite3.connect(":memory:")
     df1.to_sql("table1", conn, index=False, if_exists="replace")
     df2.to_sql("table2", conn, index=False, if_exists="replace")
     df3.to_sql("table3", conn, index=False, if_exists="replace")
+    df4.to_sql("table4", conn, index=False, if_exists="replace")
+    df5.to_sql("table5", conn, index=False, if_exists="replace")
 
     # SQL Query 1
     summary_query1 = """
 
 WITH base1 AS (SELECT "Current Mentor", 
-    COUNT(DISTINCT "ADEK Applicant ID") AS "No. of Students Allocated" 
+    COUNT(DISTINCT "ADEK Applicant ID") AS "No. of Students" 
     FROM table2
     GROUP BY 1),
 base2 AS (SELECT "Logged by",
@@ -37,21 +41,24 @@ base2 AS (SELECT "Logged by",
     SUM(CASE WHEN "Billable / Non Billable" = "Non Billable" THEN "Duration in hours" ELSE 0 END) OVER (PARTITION BY "Logged by") AS "Non Billable Hours"
     FROM table1)
 SELECT DISTINCT b2."Logged by" AS Mentor,
+    t5."Mentor Status",
     b2."Team Lead",
-    b1."No. of Students Allocated",
-    b1."No. of Students Allocated"*2.5 AS "Standard Hour / Student (2.5 hours)",
+    b1."No. of Students",
+    b1."No. of Students"*2.5 AS "Standard Hour / Student (2.5 hours)",
     '' AS "No. of Student Transitioned",
     '' AS "Allocated hours for transition (1.5 hours)",
     '' AS "Total Time",
     "Billable Hours" AS "Billable",
     "Non Billable Hours" AS "Non Billable",
     "Billable Hours" + "Non Billable Hours" AS Total,
-    '' AS "Hours to be paid for October 2025",
-    '' AS "Hours to be paid for September 2025",
+    '' AS "Hours to be paid for Aug 2025",
+    '' AS "Hours to be paid for July 2025",
     '' AS "Total Payment",
     '' AS "HQ Remark"
     FROM base2 b2 LEFT JOIN base1 b1 
-        ON b2."Logged by" = b1."Current Mentor";
+        ON b2."Logged by" = b1."Current Mentor"
+                  LEFT JOIN table5 t5
+        ON b2."Logged by" = t5."Mentor";
 
     """
 
@@ -63,17 +70,21 @@ SELECT DISTINCT b2."Logged by" AS Mentor,
         t2.Country,
         SUM(t1."Duration in hours") AS "Advising Hours"
     FROM table2 t2 LEFT JOIN table1 t1 
-      ON t1."PS Number" = t2."ADEK Applicant ID"
+        ON t1."PS Number" = t2."ADEK Applicant ID"
     GROUP BY 1,2,3)
     
     SELECT b1."ADEK Applicant ID",
     b1."Student Name",
+    t3."Mentor Name",
+    t3."Team Leader Name",
     b1."Country",
-    t3."Date of meeting with student",
+    CASE WHEN t3."Date of meeting with student" > t4."Date of meeting with student" THEN t3."Date of meeting with student" ELSE t4."Date of meeting with student" END AS "Last Date of meeting with student",
     CASE WHEN LENGTH(TRIM("Advising Hours")) > 0 THEN "Advising Hours" 
       ELSE 0 END AS "Advising Hours" 
     FROM base1 b1 LEFT JOIN table3 t3
-      ON b1."ADEK Applicant ID" = t3."Student ADEK Application ID";
+        ON b1."ADEK Applicant ID" = t3."Student ADEK Application ID"
+                  LEFT JOIN table4 t4
+        ON b1."ADEK Applicant ID" = t4."Student ADEK Application ID";
     
     """
 
@@ -100,6 +111,3 @@ SELECT DISTINCT b2."Logged by" AS Mentor,
         file_name="Payroll File.xlsx",
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
-
-
-
