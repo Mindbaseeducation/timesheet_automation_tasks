79eac0c34ae7342414e22b68020b2e144bd3c7f4
Mindbaseeducation <arjun.v@mindbase.education>
2026-03-03 17:02:50 +0530

    Update tsa_student_summary.py
    Removed column "Allocated hours for transition (1.5 hours)"

--- tsa_student_summary.py
+++ tsa_student_summary.py
@@ -46,7 +46,6 @@ SELECT DISTINCT b2."Logged by" AS Mentor,
     b1."No. of Students",
     b1."No. of Students"*2.5 AS "Standard Hour / Student (2.5 hours)",
     '' AS "No. of Student Transitioned",
-    '' AS "Allocated hours for transition (1.5 hours)",
     '' AS "Total Time",
     "Billable Hours" AS "Billable",
     "Non Billable Hours" AS "Non Billable",
@@ -112,6 +111,7 @@ SELECT DISTINCT b2."Logged by" AS Mentor,
         file_name="Payroll File.xlsx",
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
+
 
 
 
