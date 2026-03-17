942709e6b8cc809cb3e560b6c44edf5a0421f47a
Mindbaseeducation <arjun.v@mindbase.education>
2026-03-17 21:57:04 +0530

    Update tsa_task2.py
    Updated months as per "March" timesheet.

--- tsa_task2.py
+++ tsa_task2.py
@@ -43,9 +43,9 @@ base4 AS (SELECT "PS Number",
         "Category",
         "Logged by",
         "Team Lead",
-         CASE WHEN LOWER("Entry Label") LIKE '%dec%' 
-             OR LOWER("Entry Label") LIKE '%jan%' 
-             OR LOWER("Entry Label") LIKE '%mar%' THEN "Task pertaining to previous or next month will not be paid"
+         CASE WHEN LOWER("Entry Label") LIKE '%jan%' 
+             OR LOWER("Entry Label") LIKE '%feb%' 
+             OR LOWER("Entry Label") LIKE '%apr%' THEN "Task pertaining to previous or next month will not be paid"
             WHEN "Duration in hours" > 12 THEN "Please rectify the AM/PM Error"
             ELSE "Audit Remark" END AS "Audit Remark",
         "TL Remark"
